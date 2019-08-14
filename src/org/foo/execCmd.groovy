c67a07885be207661420b66c5deb16fd81641b7a
hcchen20022002 <hcchen20022002@gmail.com>
2019-08-14 15:19:24 +0800

    Update execCmd.groovy

--- src/org/foo/execCmd.groovy
+++ src/org/foo/execCmd.groovy
@@ -1,4 +1,11 @@
 package org.foo;
-def callCmd(cmd) {
-        echo(cmd)
+def call(cmd, execPath=null) {
+    echo "[Command]: "+cmd
+    def sout = new StringBuffer()
+    def serr = new StringBuffer()
+    def proc = cmd.execute(null, execPath)
+    proc.consumeProcessOutput(sout, serr)
+    proc.waitFor();
+    if(sout) echo "[Output]: "+sout.toString().trim()
+    if(serr) error serr.toString().trim()
 }
