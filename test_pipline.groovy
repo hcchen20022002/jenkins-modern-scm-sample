4ff2f02160238a87b15e578fbb8ba3ed7edc17ce
hcchen20022002 <hcchen20022002@gmail.com>
2021-12-09 11:10:50 +0800

    Update test_pipline.groovy

--- test_pipline.groovy
+++ test_pipline.groovy
@@ -2,6 +2,6 @@ node {
     // "test" is a globle lib name on jenkins setting
     //@Library("test") _
 
-    def e = new org.foo.execCmd()
-    e.call("echo 123")
+    //def e = new org.foo.execCmd()
+    ssh "echo 123"
 }
